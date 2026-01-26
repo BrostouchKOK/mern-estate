ac49540e44fe4cf7e6de0196dcbdfde6d8ff5a96
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-01-26 17:50:07 +0700

    create and run the server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Server is nuning on port 3000!!")
+})
+
+
+
+
+// mv .git ../    ==>use to move git to root directory
