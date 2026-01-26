146a3ec343f4f86316413f325310024bc05f1e4e
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-01-26 18:39:40 +0700

    conect to database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server is nuning on port 3000!!")
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
-
-
+app.listen(3000, () => {
+  console.log("Server is nuning on http://localhost:3000");
+});
 
 // mv .git ../    ==>use to move git to root directory
+// password: IT3
+// email: IT3.Dev
