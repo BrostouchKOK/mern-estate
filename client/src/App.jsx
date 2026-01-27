49c55422fa75234efa7f4c80ac7cdf822f7d8f59
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-01-27 18:27:44 +0700

    complete sign up page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,34 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,26 +1,95 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(true);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return
+      }
+      setLoading(false);
+      setError(null)
+
+      if(data.success === true){
+        toast.success(data.message);
+        navigate("/sign-in")
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+
+    }
+  };
+
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center my-7 font-bold'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
+    <div className="py-5 px-7 max-w-lg mx-auto mt-7 rounded-md shadow-lg">
+      <h1 className="text-3xl text-center font-bold">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 my-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="p-3 rounded-lg border outline-none"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="p-3 rounded-lg border outline-none"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="p-3 rounded-lg border outline-none"
+          onChange={handleChange}
+        />
 
-        <input type="text" placeholder='username' id='username' className='p-3 rounded-lg border outline-none' />
-        <input type="email" placeholder='email' id='email' className='p-3 rounded-lg border outline-none' />
-        <input type="password" placeholder='password' id='password' className='p-3 rounded-lg border outline-none' />
-
-        <button className='uppercase p-3 rounded-lg bg-blue-500 text-white hover:opacity-95 disabled:opacity-80 cursor-pointer'>Sign Up</button>
+        <button className="uppercase p-3 rounded-lg bg-blue-500 text-white hover:opacity-95 disabled:opacity-80 cursor-pointer">
+          Sign Up
+        </button>
       </form>
 
-      <div className="flex gap-2 mt-5">
+      <div className="flex gap-2 my-5">
         <p>Have an account?</p>
-        <Link>
+        <Link to={"/"}>
           <span className="text-blue-700 hover:underline">Sign in</span>
         </Link>
       </div>
+      {
+        error && <p className="text-red-500 mt-5">{error}</p>
+      }
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
