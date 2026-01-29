13d7b511f317797dfa36040fc1fe8c737c639647
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-01-29 11:53:06 +0700

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -22,6 +23,35 @@ export const signup = async (req, res, next) => {
       message: "User created successfully",
       success: true,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return res
+        .status(404)
+        .json({ message: "User not found!", success: false });
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(401).json({
+        message: "Invalid Credentials!",
+        success: false,
+      });
+    }
+
+    // create token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
